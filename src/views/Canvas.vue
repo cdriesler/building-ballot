0437bcbab33bc6b2c07fcc135fb1fc612d4708be
Arif Hanif <ahanif@aeieng.com>
2019-10-19 20:52:08 -0400

    add a canvas component with gltf viewer

---
+++ src/views/Canvas.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <model-gltf :src="filePath"></model-gltf>  
+    <button @click="pickModel('option1')">Option 1</button>
+    <button @click="pickModel('option2')">Option 2</button>
+    <button @click="pickModel('option3')">Option 3</button>
+</div>
+</template> 
+
+<script lang="ts">
+    import { ModelGltf } from 'vue-3d-model'
+
+    export default {
+        components: {
+            ModelGltf
+        },
+        data() {
+            return {
+                modelName: "option1"
+            }
+        },
+        computed: {
+            filePath(): string{
+                return `./${this.modelName}.gltf`
+            }
+        },
+        methods: {
+            pickModel(name: string): void {
+                this.modelName = name;
+            }
+        }
+    }
+</script>
+
